bdc70736f504fdf75cc1d528c1ad56a954ab603e
Joshua Kyle Trudell <jtrudell@mail.com>
2011-06-26 20:56:12 -0400

    0aa

---
+++ src/sjmatthews/sjmEconomy/sjmEconomyBank.java
@@ -0,0 +1,19 @@
+package sjmatthews.sjmEconomy;
+
+public class sjmEconomyBank {
+	public void bankBalance() {
+
+	}
+
+	public void bankInitialize() {
+
+	}
+
+	public void bankOpenAccount() {
+
+	}
+
+	public void bankPay() {
+
+	}
+}
